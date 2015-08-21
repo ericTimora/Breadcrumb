8c0cf2b8234aa44596d0d0de978a79314f89d12b
ericTimora <eric.timora@gmail.com>
2015-08-21 16:34:55 -0700

    Location updates added

--- app/src/main/java/com/example/timora/breadcrumb/MapsActivity.java
+++ app/src/main/java/com/example/timora/breadcrumb/MapsActivity.java
@@ -8,6 +8,9 @@ import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -23,11 +26,16 @@ public class MapsActivity extends FragmentActivity implements
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private GoogleApiClient mGoogleApiClient;
     public static final String TAG = MapsActivity.class.getSimpleName();
+    private LocationRequest mLocationRequest;
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
 
     private void handleNewLocation(Location location) {
         Log.d(TAG, location.toString());
+
+        LatLng mLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate mCameraUpdate = CameraUpdateFactory.newLatLngZoom(mLatLng, 10);
+        mMap.animateCamera(mCameraUpdate);
     }
 
     @Override
@@ -35,11 +43,18 @@ public class MapsActivity extends FragmentActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        mMap.setMyLocationEnabled(true);
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(20000)        // 20 seconds, in milliseconds
+                .setFastestInterval(10000); // 10 second, in milliseconds
     }
 
     @Override
@@ -53,6 +68,7 @@ public class MapsActivity extends FragmentActivity implements
     protected void onPause() {
         super.onPause();
         if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
             mGoogleApiClient.disconnect();
         }
     }
@@ -91,6 +107,7 @@ public class MapsActivity extends FragmentActivity implements
      * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
+
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
@@ -100,7 +117,7 @@ public class MapsActivity extends FragmentActivity implements
         Log.i(TAG, "Location services connected.");
         Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (location == null) {
-            // Blank for a moment...
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         }
         else {
             handleNewLocation(location);
